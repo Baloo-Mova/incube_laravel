2eaf3fbc35d45c3e9cded0d09a7eb9f126d8224a
VlaD77 <ternovoyvladislav@mail.ru>
2016-08-21 00:30:51 +0300

    fix(not end) header menu

--- resources/views/frontend/layouts/partials/header.blade.php
+++ resources/views/frontend/layouts/partials/header.blade.php
@@ -27,120 +27,120 @@
     </div>
 
     <nav class="navbar navbar-default">  <!--navbar-fixed-top-->
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="{{url('/')}}"><img src="../img/logo.png" alt=""></a>
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="{{url('/')}}"><img src="../img/logo.png" alt=""></a>
+        </div>
+
+        @if(!Auth::check())
+        @include('frontend.layouts.partials.registered_buttons')
+        @endif
+
+        <div class="collapse navbar-collapse js-navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="main-nav nav navbar-nav navbar-center">
+                <li class="" data-wow-delay="0s">
+                    <a class="{{ (Request::is('/') ? 'active' : '') }}" href="{{url('/')}}">Головна</a></li>
+                <li class=" dropdown mega-dropdown">
+                    <a href="#" class="dropdown-toggle  {{ (Request::is('/projects') ? 'active' : '') }}" data-wow-delay="0s" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Проекти
+                        <span class="caret"></span></a>
+                    <ul class="dropdown-menu mega-dropdown-menu">
+                        <li class="col-sm-2">
+                            <ul>
+                                <li class="dropdown-header">Нові проекти</li>
+                                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                                    <div class="carousel-inner">
+                                    </div><!-- End Carousel Inner -->
+                                </div><!-- /.carousel -->
+                                <li class="divider"></li>
+                                <li><a href="{{url('/project-viewer/index')}}">Усі проекти</a></li>
+                            </ul>
+                        </li>
+                        <li class="col-sm-3">
+                            <ul>
+                                <li>
+                                    <a class="dropdown-header">Сільське господарство, лісове господарство та рибне господарство</a>
+                                </li>
+                                <li class="divider"></li>
+                                <li><a class="dropdown-header">Переробна промисловість (Виробництво)</a></li>
+                                <li class="divider"></li>
+                                <li>
+                                    <a class="dropdown-header">Водопостачання, каналізація, поводження з відходами</a>
+                                </li>
+                                <li class="divider"></li>
+                                <li><a class="dropdown-header">Будівництво</a></li>
+                                <li class="divider"></li>
+                            </ul>
+                        </li>
+                        <li class="col-sm-3">
+                            <ul>
+                                <li>
+                                    <a class="dropdown-header">Транспорт, складське господарство, поштова та кур'єрська діяльність</a>
+                                </li>
+                                <li class="divider"></li>
+                                <li><a class="dropdown-header">Професійна, наукова та технічна діяльність</a></li>
+                                <li class="divider"></li>
+                                <li>
+                                    <a class="dropdown-header">Діяльність у сфері адміністративного та допоміжного обслуговування</a>
+                                </li>
+                                <li class="divider"></li>
+                                <li>
+                                    <a class="dropdown-header">Державне управління й оборона, обов'язкове соціальне страхування</a>
+                                </li>
+                                <li class="divider"></li>
+                            </ul>
+                        </li>
+                        <li class="col-sm-3">
+                            <ul>
+                                <li><a class="dropdown-header">Освіта</a></li>
+                                <li class="divider"></li>
+                                <li><a class="dropdown-header">Охорона здоров'я та надання соціальної допомоги</a>
+                                </li>
+                                <li class="divider"></li>
+                                <li><a class="dropdown-header">Мистецтво, спорт, розваги та відпочинок</a></li>
+                                <li class="divider"></li>
+                                <li>
+                                    <a class="dropdown-header">Надання інших видів послуг (інші види діяльності) </a>
+                                </li>
+                                <li class="divider"></li>
+                            </ul>
+                        </li>
+
+                    </ul>
+                </li>
+
+                <li class="" data-wow-delay="0.2s"><a href="{{url('/investor/index')}}">Інвестору</a></li>
+                <li class="" data-wow-delay="0.2s"><a href="{{url('/customer/index')}}">Замовнику</a></li>
+                <li class="" data-wow-delay="0.2s"><a href="{{url('/designer/index')}}">Проектанту</a></li>
+                <li class="" data-wow-delay="0.3s"><a href="{{url('/executor/index')}}">Виконавцю</a></li>
+                <li class="" data-wow-delay="0.4s"><a href="{{url('/site/contact')}}">Контакти</a></li>
+
+            </ul>
+            <div class="button navbar-right">
+                @if(Auth::check())
+                <a href="{{url('/personal-area')}}" class="btn navbar-btn nav-button wow bounceInRight login">
+                    <span class="glyphicon glyphicon-home"></span> Особистий кабінет
+                </a>
+                @endif
             </div>
-
-            @if(!Auth::check())
-                @include('frontend.layouts.partials.registered_buttons')
-            @endif
-
-            <div class="collapse navbar-collapse js-navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="main-nav nav navbar-nav navbar-center">
-                    <li class="" data-wow-delay="0s">
-                        <a class="{{ (Request::is('/') ? 'active' : '') }}" href="{{url('/')}}">Головна</a></li>
-                    <li class=" dropdown mega-dropdown">
-                        <a href="#" class="dropdown-toggle  {{ (Request::is('/projects') ? 'active' : '') }}" data-wow-delay="0s" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Проекти
-                            <span class="caret"></span></a>
-                        <ul class="dropdown-menu mega-dropdown-menu">
-                            <li class="col-sm-2">
-                                <ul>
-                                    <li class="dropdown-header">Нові проекти</li>
-                                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                                        <div class="carousel-inner">
-                                        </div><!-- End Carousel Inner -->
-                                    </div><!-- /.carousel -->
-                                    <li class="divider"></li>
-                                    <li><a href="{{url('/project-viewer/index')}}">Усі проекти</a></li>
-                                </ul>
-                            </li>
-                            <li class="col-sm-3">
-                                <ul>
-                                    <li>
-                                        <a class="dropdown-header">Сільське господарство, лісове господарство та рибне господарство</a>
-                                    </li>
-                                    <li class="divider"></li>
-                                    <li><a class="dropdown-header">Переробна промисловість (Виробництво)</a></li>
-                                    <li class="divider"></li>
-                                    <li>
-                                        <a class="dropdown-header">Водопостачання, каналізація, поводження з відходами</a>
-                                    </li>
-                                    <li class="divider"></li>
-                                    <li><a class="dropdown-header">Будівництво</a></li>
-                                    <li class="divider"></li>
-                                </ul>
-                            </li>
-                            <li class="col-sm-3">
-                                <ul>
-                                    <li>
-                                        <a class="dropdown-header">Транспорт, складське господарство, поштова та кур'єрська діяльність</a>
-                                    </li>
-                                    <li class="divider"></li>
-                                    <li><a class="dropdown-header">Професійна, наукова та технічна діяльність</a></li>
-                                    <li class="divider"></li>
-                                    <li>
-                                        <a class="dropdown-header">Діяльність у сфері адміністративного та допоміжного обслуговування</a>
-                                    </li>
-                                    <li class="divider"></li>
-                                    <li>
-                                        <a class="dropdown-header">Державне управління й оборона, обов'язкове соціальне страхування</a>
-                                    </li>
-                                    <li class="divider"></li>
-                                </ul>
-                            </li>
-                            <li class="col-sm-3">
-                                <ul>
-                                    <li><a class="dropdown-header">Освіта</a></li>
-                                    <li class="divider"></li>
-                                    <li><a class="dropdown-header">Охорона здоров'я та надання соціальної допомоги</a>
-                                    </li>
-                                    <li class="divider"></li>
-                                    <li><a class="dropdown-header">Мистецтво, спорт, розваги та відпочинок</a></li>
-                                    <li class="divider"></li>
-                                    <li>
-                                        <a class="dropdown-header">Надання інших видів послуг (інші види діяльності) </a>
-                                    </li>
-                                    <li class="divider"></li>
-                                </ul>
-                            </li>
-
-                        </ul>
-                    </li>
-
-                    <li class="" data-wow-delay="0.2s"><a href="{{url('/investor/index')}}">Інвестору</a></li>
-                    <li class="" data-wow-delay="0.2s"><a href="{{url('/customer/index')}}">Замовнику</a></li>
-                    <li class="" data-wow-delay="0.2s"><a href="{{url('/designer/index')}}">Проектанту</a></li>
-                    <li class="" data-wow-delay="0.3s"><a href="{{url('/executor/index')}}">Виконавцю</a></li>
-                    <li class="" data-wow-delay="0.4s"><a href="{{url('/site/contact')}}">Контакти</a></li>
-
-                </ul>
-                <div class="button navbar-right">
-                    @if(Auth::check())
-                        <a href="{{url('/personal-area')}}" class="btn navbar-btn nav-button wow bounceInRight login">
-                            <span class="glyphicon glyphicon-home"></span> Особистий кабінет
-                        </a>
-                    @endif
-                </div>
-            </div><!-- /.navbar-collapse -->
+        </div><!-- /.navbar-collapse -->
     </nav>
 </div>
 {{--<div id="myModal" class="modal fade">--}}
-    {{--<div class="modal-dialog">--}}
-        {{--<div class="modal-content">--}}
-            {{--<div class="modal-header">--}}
-                {{--<button type="button" class="close" data-dismiss="modal">x</button>--}}
-                {{--<div class="row-fluid user-row">--}}
-                    {{--<img src="../img/logo.png" class="img-responsive" alt="log"/>--}}
-                {{--</div>--}}
-                {{--<h3 style="text-align: center;">Реєстрація</h3>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</div>--}}
+{{--<div class="modal-dialog">--}}
+{{--<div class="modal-content">--}}
+{{--<div class="modal-header">--}}
+{{--<button type="button" class="close" data-dismiss="modal">x</button>--}}
+{{--<div class="row-fluid user-row">--}}
+{{--<img src="../img/logo.png" class="img-responsive" alt="log"/>--}}
+{{--</div>--}}
+{{--<h3 style="text-align: center;">Реєстрація</h3>--}}
+{{--</div>--}}
+{{--</div>--}}
+{{--</div>--}}
 {{--</div>--}}
