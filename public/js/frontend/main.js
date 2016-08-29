061453cced145d33ebd903c23d87a8a50218be28
VlaD77 <ternovoyvladislav@mail.ru>
2016-08-30 02:07:02 +0300

    add logo businessman  refresh header update index add script auto-height  for how-it-work panel

--- public/js/frontend/main.js
+++ public/js/frontend/main.js
@@ -19,3 +19,22 @@ $(document).ready(function() {
 });
 
  new WOW().init();
+ 
+ function setEqualHeight(columns)
+{
+var tallestcolumn = 0;
+columns.each(
+function()
+{
+currentHeight = $(this).height();
+if(currentHeight > tallestcolumn)
+{
+tallestcolumn = currentHeight;
+}
+}
+);
+columns.height(tallestcolumn);
+}
+$(document).ready(function() {
+setEqualHeight($(".how-it-work > a > div > div > p"));
+});
