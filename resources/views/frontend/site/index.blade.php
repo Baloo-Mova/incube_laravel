ecbbee83b5ffef6dd8a6b3a2eae384976e8caa82
VlaD77 <ternovoyvladislav@mail.ru>
2016-08-29 21:47:42 +0300

    new slider product element

--- resources/views/frontend/site/index.blade.php
+++ resources/views/frontend/site/index.blade.php
@@ -256,16 +256,16 @@
                                                     for ($i = 0; $i < 10; $i++) {
                                                     ?>
                                                     <li>
-                                                        <figure>
+                                                        <div class="figure">
                                                             <a class="aa-product-img" href="#"><img src="../img/250n300.png" alt="polo shirt img"></a>
                                                             <a class="aa-add-card-btn" href="#"><span class="glyphicon glyphicon-arrow-right"></span>Продивитись</a>
-                                                            <figcaption class="aa-content">
+                                                            <div class="aa-content">
                                                                 <h4 class="aa-product-title">Питання </h4>
                                                             <!--<span class="aa-product-price">$<?= number_format(rand(60,
                                                                     662000), 0, '.',
                                                                     '.') ?></span><span class="aa-product-price"><del>$65.50</del></span>-->
-                                                            </figcaption>
-                                                        </figure>
+                                                            </div>
+                                                        </div>
 
                                                         <!-- product id -->
                                                         <span class="aa-badge aa-sale" href="#"><?= $i + 1 ?></span>
