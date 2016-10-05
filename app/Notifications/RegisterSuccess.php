37669c862bb80bcc29123de78079a5294b55c775
Sergey <mova_sa@groupbwt.com>
2016-10-05 14:12:19 +0300

    notify fix

--- app/Notifications/RegisterSuccess.php
+++ app/Notifications/RegisterSuccess.php
@@ -32,7 +32,7 @@ class RegisterSuccess extends Notification implements ShouldQueue
      */
     public function via($notifiable)
     {
-        return ['mail'];
+        return ['mail','database'];
     }
 
     /**
