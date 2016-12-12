aa910eb762b9ccc552d0650bfa96dce3e2e26403
VlaD77 <ternovoyvladislav@mail.ru>
2016-12-12 20:38:37 +0200

    fixes

--- resources/views/admin/layouts/partials/header.blade.php
+++ resources/views/admin/layouts/partials/header.blade.php
@@ -1,10 +1,11 @@
+<div id="preloader"></div>
 <div id="header" class="navbar-fixed-top" role="navigation">
     <div class="header-connect">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-5 col-sm-8 col-xs-12 text-center">
                     <div class="header-call">
-                        <i class="glyphicon glyphicon-earphone"></i><span>  +777 7777 7777</span>
+                        <i class="glyphicon glyphicon-earphone"></i><span>  +38(061) 228-75-82</span>
                         <i class="glyphicon glyphicon-envelope"></i><span> incube.zp@gmail.com</span>
                     </div>
                 </div>
@@ -13,7 +14,7 @@
                         <ul class="list-inline">
                             <li><a href="#"><i class="fa fa-facebook"></i></a></li>
                             <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fa fa-vine"></i></a></li>
+                            <li><a href="https://vk.com/projectznu"><i class="fa fa-vk"></i></a></li>
                             <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
                             <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
                             <li><a href="#"><i class="fa fa-instagram"></i></a></li>
@@ -44,7 +45,7 @@
                         <li class=""><a href="{{url('/login')}}">Вхід</a></li>
                     @else
                         <li class=""><a href="{{ url('/personal-area') }}">Особистий кабінет <span
-                                        class="event label label-danger">{{ Auth::check()? (count(Auth::user()->unreadNotifications) > 0 ? count(Auth::user()->unreadNotifications) : '')  : '' }}</span></a></li>
+                                        class="event label label-danger">{{ Auth::check() ? (count(Auth::user()->unreadNotifications) > 0 ? count(Auth::user()->unreadNotifications) : '')  : '' }}</span></a></li>
                         <li class="">
                             <a href="{{ url('/logout') }}"
                                onclick="event.preventDefault();
@@ -83,56 +84,59 @@
                                 <li class="col-sm-3">
                                     <ul>
                                         <li>
-                                            <a class="dropdown-header">Сільське господарство, лісове господарство та
-                                                рибне господарство</a>
+                                            <a class="dropdown-header" href="{{url('/project-viewer?cat_id=2')}}">Сільське господарство, лісове господарство та рибне господарство</a>
                                         </li>
-                                        <li class="divider"></li>
-                                        <li><a class="dropdown-header">Переробна промисловість (Виробництво)</a></li>
                                         <li class="divider"></li>
                                         <li>
-                                            <a class="dropdown-header">Водопостачання, каналізація, поводження з
-                                                відходами</a>
+                                            <a class="dropdown-header" href="{{url('/project-viewer?cat_id=3')}}">Переробна промисловість (Виробництво)</a>
                                         </li>
                                         <li class="divider"></li>
-                                        <li><a class="dropdown-header">Будівництво</a></li>
+                                        <li>
+                                            <a class="dropdown-header" href="{{url('/project-viewer?cat_id=4')}}">Водопостачання, каналізація, поводження з відходами</a>
+                                        </li>
+                                        <li class="divider"></li>
+                                        <li>
+                                            <a class="dropdown-header" href="{{url('/project-viewer?cat_id=5')}}">Будівництво</a>
+                                        </li>
                                         <li class="divider"></li>
                                     </ul>
                                 </li>
                                 <li class="col-sm-3">
                                     <ul>
                                         <li>
-                                            <a class="dropdown-header">Транспорт, складське господарство, поштова та
-                                                кур'єрська діяльність</a>
-                                        </li>
-                                        <li class="divider"></li>
-                                        <li><a class="dropdown-header">Професійна, наукова та технічна діяльність</a>
+                                            <a class="dropdown-header" href="{{url('/project-viewer?cat_id=6')}}">Транспорт, складське господарство, поштова та кур'єрська діяльність</a>
                                         </li>
                                         <li class="divider"></li>
                                         <li>
-                                            <a class="dropdown-header">Діяльність у сфері адміністративного та
-                                                допоміжного обслуговування</a>
+                                            <a class="dropdown-header" href="{{url('/project-viewer?cat_id=8')}}">Професійна, наукова та технічна діяльність</a>
                                         </li>
                                         <li class="divider"></li>
                                         <li>
-                                            <a class="dropdown-header">Державне управління й оборона, обов'язкове
-                                                соціальне страхування</a>
+                                            <a class="dropdown-header" href="{{url('/project-viewer?cat_id=9')}}">Діяльність у сфері адміністративного та допоміжного обслуговування</a>
+                                        </li>
+                                        <li class="divider"></li>
+                                        <li>
+                                            <a class="dropdown-header" href="{{url('/project-viewer?cat_id=10')}}">Державне управління й оборона, обов'язкове соціальне страхування</a>
                                         </li>
                                         <li class="divider"></li>
                                     </ul>
                                 </li>
                                 <li class="col-sm-3">
                                     <ul>
-                                        <li><a class="dropdown-header">Освіта</a></li>
-                                        <li class="divider"></li>
-                                        <li><a class="dropdown-header">Охорона здоров'я та надання соціальної
-                                                допомоги</a>
+                                        <li>
+                                            <a class="dropdown-header" href="{{url('/project-viewer?cat_id=11')}}">Освіта</a>
                                         </li>
                                         <li class="divider"></li>
-                                        <li><a class="dropdown-header">Мистецтво, спорт, розваги та відпочинок</a></li>
+                                        <li>
+                                            <a class="dropdown-header" href="{{url('/project-viewer?cat_id=12')}}">Охорона здоров'я та надання соціальної допомоги</a>
+                                        </li>
                                         <li class="divider"></li>
                                         <li>
-                                            <a class="dropdown-header">Надання інших видів послуг (інші види
-                                                діяльності) </a>
+                                            <a class="dropdown-header" href="{{url('/project-viewer?cat_id=13')}}">Мистецтво, спорт, розваги та відпочинок</a>
+                                        </li>
+                                        <li class="divider"></li>
+                                        <li>
+                                            <a class="dropdown-header" href="{{url('/project-viewer?cat_id=14')}}">Надання інших видів послуг (інші види діяльності) </a>
                                         </li>
                                         <li class="divider"></li>
                                     </ul>
@@ -144,38 +148,33 @@
                                data-wow-delay="0s" data-toggle="dropdown">
                                 Користувачу<b class="caret"></b>
                             </a>
-
                             <ul class="dropdown-menu mega-dropdown-menu">
-                                <li class="col-sm-2 col-sm-offset-1 text-center">
-                                    <a href="{{url('/customer')}}" class="dropdown-header">
+                                <li class="col-sm-3 text-center">
+                                    <a href="{{route('problem.index')}}" class="dropdown-header">
                                         <div class="hidden-xs">
                                             <img src="{{url('/img/'.'how-work3.png')}}" alt="">
-
                                         </div>
                                         Замовнику
                                     </a>
                                 </li>
-                                <li class="col-sm-2 text-center">
-                                    <a href="{{url('/investor')}}" class="dropdown-header">
+                                <li class="col-sm-3 text-center">
+                                    <a href="{{route('investor.index')}}" class="dropdown-header">
                                         <div class="hidden-xs">
                                             <img src="{{url('/img/'.'investor_desc_logo.png')}}" alt="">
-
                                         </div>
                                         Інвестору
                                     </a>
                                 </li>
-                                <li class="col-sm-2 text-center">
-                                    <a href="{{url('/designer')}}" class="dropdown-header">
+                                <li class="col-sm-3 text-center">
+                                    <a href="{{route('designer.index')}}" class="dropdown-header">
                                         <div class="hidden-xs">
                                             <img src="{{url('/img/'.'how-work1.png')}}" alt="">
-
                                         </div>
-
                                         Проектанту
                                     </a>
                                 </li>
-                                <li class="col-sm-2 text-center">
-                                    <a href="{{url('/executor')}}" class="dropdown-header">
+                                <li class="col-sm-3 text-center">
+                                    <a href="{{route('executor.index')}}" class="dropdown-header">
                                         <div class="hidden-xs">
                                             <img src="{{url('/img/'.'how-work2.png')}}" alt="">
 
@@ -183,24 +182,24 @@
                                         Виконавцю
                                     </a>
                                 </li>
-                                <li class="col-sm-2 text-center">
-                                    <a href="{{url('/employer')}}" class="dropdown-header">
+                                {{--<li class="col-sm-2 text-center">--}}
+                                    {{--<a href="{{url('/employer')}}" class="dropdown-header">--}}
 
-                                        <div class="hidden-xs">
-                                            <img src="{{url('/img/'.'how-work5.png')}}" alt="">
+                                        {{--<div class="hidden-xs">--}}
+                                            {{--<img src="{{url('/img/'.'how-work5.png')}}" alt="">--}}
 
-                                        </div>
-                                        Підприємцю
-                                    </a>
-                                </li>
-
+                                        {{--</div>--}}
+                                        {{--Підприємцю--}}
+                                    {{--</a>--}}
+                                {{--</li>--}}
                             </ul>
 
                         </li>
-                        <li><a href="{{url('/contacts')}}">Контакти</a></li>
-                        <li><a href="{{url('/about')}}">Про нас</a></li>
-                        <li><a href="{{url('/rules')}}">Правила</a></li>
-                        <li><a href="{{url('/blog')}}">Блог</a></li>
+                        <li><a href="{{url('/contacts')}}" class="{{ (Request::is('contacts') ? 'active' : '') }}">Контакти</a></li>
+                        <li><a href="{{url('/about')}}" class="{{ (Request::is('about') ? 'active' : '') }}">Про нас</a></li>
+                        <li><a href="{{url('/rules')}}" class="{{ (Request::is('rules') ? 'active' : '') }}">Правила</a></li>
+                        <li><a href="{{url('/blog')}}" class="{{ (Request::is('blog') ? 'active' : '') }}">Конкурси та гранти</a></li>
+                        <li><a href="{{url('/news')}}" class="{{ (Request::is('news') ? 'active' : '') }}">Новини</a></li>
                     </ul>
                 </div>
             </div><!-- /.navbar-collapse -->
