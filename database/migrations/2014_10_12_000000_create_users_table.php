b2a36899bf84ca769dc3cceda4ba936d0494f908
Vladyslav Ternovoi <vladislav.ternovoy@meliorgames.com>
2017-10-30 18:42:51 +0200

    work in progress

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->integer('user_type_id')->default(0);
             $table->string('password');
+            $table->string('country');
             $table->rememberToken();
             $table->timestamps();
         });
